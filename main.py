c9b310d51bd7dd02c5706c1092d3f60fc5163aa6
Mariana Lopez Jaimez <mariana.lopez.jaimez@gmail.com>
2015-07-08 15:48:59 -0700

    Adding a thank you page to use when the Spoogler completes the registration form, adding the corresponding handlers and methods to send an email in app.yaml and main.py

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import webapp2
 import os
 import jinja2
 
-import random
-
 from models import Spoogler
 from google.appengine.api import mail
+from random import randint
 
 jinja_env = jinja2.Environment(loader = 
 jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -46,6 +45,9 @@ class FormHandler(webapp2.RequestHandler):
     
     return template.render(context)
   
+  def _generate_token(self):
+    return randint(111111,999999)
+  
 class ConfirmHandler(webapp2.RequestHandler):
   def get(self):
     template_context = {'user': self.request.get('u'),
@@ -62,15 +64,27 @@ class ConfirmHandler(webapp2.RequestHandler):
     return template.render(context)
 
 class EmailHandler(webapp2.RequestHandler):
-  def post(self):
+  def get(self):
     email = "mlopezj14@gmail.com"
     
     sender_address = "Spooglers Webmaster <bayareaspooglers.webmaster@gmail.com>"
     subject = "Test"
     body = "Test email"
     mail.send_mail(sender_address, email, subject, body)
+    template_context = {}
+    self.response.out.write(self._render_template('thankyou.html', 
+                            template_context))
+                                
+  def _render_template(self, template_name, context=None):
+    if context is None:
+      context = {}
+    
+    template = jinja_env.get_template(template_name)
+    
+    return template.render(context)
     
 app = webapp2.WSGIApplication([(r'/welcome_form\.html', FormHandler),
                                (r'/confirm\.html', ConfirmHandler),
+                               (r'/thankyou\.html', EmailHandler), 
                               ], debug = True)
 
