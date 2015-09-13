a7d705aae6bbbe7093169793a069b3636a5400e7
Mariana Lopez Jaimez <mariana.lopez.jaimez@gmail.com>
2015-09-12 18:08:39 -0700

    Adding logic to work with multiple-option questions.

--- main.py
+++ main.py
@@ -24,6 +24,17 @@ import logging
 from google.appengine.api import mail
 from google.appengine.ext import ndb
 from models import Spoogler
+from models import work_status
+from models import english_proficiency
+from models import address_options
+from models import time
+from models import transportation
+from models import side_driving
+from models import event_size
+from models import event_type
+from models import support_type
+from models import support_other
+from models import children_ages
 from random import randint
 
 JINJA_ENV = jinja2.Environment(loader=
@@ -57,7 +68,8 @@ class FormHandler(webapp2.RequestHandler):
             'spoogler_email': self.request.get('spoogler_email').strip(),
             'spoogler_fb_email': self.request.get('spoogler_fb_email').strip(),
             'googler_ldap': self.request.get('googler_ldap').strip(),
-            'work_status': self.request.params.get('work_status'),
+            'spoogler_country': self.request.get('spoogler_country').strip(),
+            'work_status': int(self.request.get('work_status')),
             'native_lang': self.request.get('native_lang').strip(),
             'spoogler_relo': self.request.get('spoogler_relo').strip(),
         }
@@ -186,18 +198,17 @@ class FormHandler(webapp2.RequestHandler):
         """Initializes the template fields that contains multiple options
         in the form."""
 
-        template_context['spoogler_country'] =  self.request.get('spoogler_country').strip()
-        template_context['spoogler_work_status'] =  self.work_status
-        template_context['engl_proficiency'] = self.english_proficiency
-        template_context['address'] = self.address_options
-        template_context['time'] = self.time
-        template_context['transportation'] = self.transportation
-        template_context['side_driving'] = self.side_driving
-        template_context['event_size'] = self.event_size
-        template_context['event_type'] = self.event_type
-        template_context['support_type']  = self.support_type
-        template_context['support_other'] = self.support_other
-        template_context['children_ages'] = self.children_ages
+        template_context['spoogler_work_status'] = work_status
+        template_context['engl_proficiency'] = english_proficiency
+        template_context['address'] = address_options
+        template_context['time'] = time
+        template_context['transportation'] = transportation
+        template_context['side_driving'] = side_driving
+        template_context['event_size'] = event_size
+        template_context['event_type'] = event_type
+        template_context['support_type'] = support_type
+        template_context['support_other'] = support_other
+        template_context['children_ages'] = children_ages
 
     @classmethod
     def _clean_context(cls, template_context):
@@ -218,54 +229,6 @@ class FormHandler(webapp2.RequestHandler):
         template_context['googler_ldap_error'] = False
 
     def __init__(self, request, response):
-        # Spoogler's current work status
-        self.work_status = [(0, "Visa allows work"), (1, "Visa does not allow work"),
-                            (2, "Able to work"), (3, "Not able to work")]
-        # Spoogler's English language proficiency
-        self.english_proficiency = [(0, "Fully fluent/native speaker"),
-                                    (1, "Almost fluent"), (2, "Conversational"),
-                                    (3, "Minimal"), (4, "None")]
-        # Spoogler's address in the Bay Area
-        self.address_options = [(0, "East Bay"), (1, "South Bay"), (2, "Peninsula"),
-                                (3, "San Francisco"), (4, "North Bay"), (5, "Other")]
-        # Time living in the Bay Area
-        self.time = [(0, "6 months or less"), (1, "Less than 1 year"), (2, "More than 1 year")]
-        # Mode of transportation
-        self.transportation = [(0, "Driving personal/family car"), (1, "Carpooling"),
-                               (2, "Public transit"), (3, "Bicycle"), (4, "Walking")]
-        # Side of the road to drive
-        self.side_driving = [(0, "left"), (1, "right")]
-        # Size of events to attend
-        self.event_size = [(0, "Small, informal local groups (i.e. coffee at \
-                           local cafe)"), (1, "Large groups (i.e. family picnics)"),
-                           (2, "Large regional get-togethers and parties for \
-                           special occasions")]
-        # Type of events to attend
-        self.event_type = [(0, "Arts and crafts"), (1, "Beauty/personal grooming"),
-                           (2, "Book club"), (3, "Career counseling (for those who \
-                           cannot work)"), (4, "Casual mid-morning coffee"),
-                           (5, "Concerts, theater"), (6, "Cooking"), (7, "Date nights, \
-                           movies"), (8, "Dinner, drinks"), (9, "Education/skills \
-                           sharing"), (10, "Family picnics"), (11, "Games, board games"),
-                           (12, "Playdates"), (13, "Sightseeing"), (14, "Sports"),
-                           (15, "Volunteering"), (16, "Other")]
-        # Type of support services Spoogler is looking for
-        self.support_type = [(0, "Local recommendations (i.e. for housing, \
-                             health specialists, schools, etc.)"),
-                             (1, "Information (i.e. on US taxes, healthcare \
-                             system, building credit, visas, etc.)"),
-                             (2, "Networking (based on home country, interests, \
-                             current residence, etc.)"),
-                             (3, "Other")]
-        # Other types of support used by Spoogler
-        self.support_other = [(0, "Family/friends here"),
-                              (1, "Country of origin or ethnicity-based groups"),
-                              (2, "Faith-based groups"), (3, "Other")]
-        # Spoogler's children ages
-        self.children_ages = [(0, "No children"), (1, "Expecting"), (2, "Infant"),
-                              (3, "Preschool, kindergarten"), (4, "Elementary school"),
-                              (5, "Middle school"), (6, "High school"),
-                              (7, "College/university"), (8, "Left home")]
         self.initialize(request, response)
 
 class ConfirmHandler(webapp2.RequestHandler):
--- models.py
+++ models.py
@@ -1,12 +1,108 @@
 from google.appengine.ext import ndb
 
+# Spoogler's current work status
+work_status = [
+    (0, "Visa allows work"),
+    (1, "Visa does not allow work"),
+    (2, "Able to work"),
+    (3, "Not able to work")]
+
+# Spoogler's English language proficiency
+english_proficiency = [
+    (0, "Fully fluent/native speaker"),
+    (1, "Almost fluent"),
+    (2, "Conversational"),
+    (3, "Minimal"),
+    (4, "None")]
+
+# Spoogler's address in the Bay Area
+address_options = [
+    (0, "East Bay"),
+    (1, "South Bay"),
+    (2, "Peninsula"),
+    (3, "San Francisco"),
+    (4, "North Bay"),
+    (5, "Other")]
+
+# Time living in the Bay Area
+time = [
+    (0, "6 months or less"),
+    (1, "Less than 1 year"),
+    (2, "More than 1 year")]
+
+# Mode of transportation
+transportation = [
+    (0, "Driving personal/family car"),
+    (1, "Carpooling"),
+    (2, "Public transit"),
+    (3, "Bicycle"),
+    (4, "Walking")]
+
+# Side of the road to drive
+side_driving = [
+    (0, "left"),
+    (1, "right")]
+
+# Size of events to attend
+event_size = [
+    (0, "Small, informal local groups (i.e. coffee at local cafe)"),
+    (1, "Large groups (i.e. family picnics)"),
+    (2, "Large regional get-togethers and parties for special occasions")]
+
+# Type of events to attend
+event_type = [
+    (0, "Arts and crafts"),
+    (1, "Beauty/personal grooming"),
+    (2, "Book club"),
+    (3, "Career counseling (for those who cannot work)"),
+    (4, "Casual mid-morning coffee"),
+    (5, "Concerts, theater"),
+    (6, "Cooking"),
+    (7, "Date nights, movies"),
+    (8, "Dinner, drinks"),
+    (9, "Education/skills sharing"),
+    (10, "Family picnics"),
+    (11, "Games, board games"),
+    (12, "Playdates"),
+    (13, "Sightseeing"),
+    (14, "Sports"),
+    (15, "Volunteering"),
+    (16, "Other")]
+
+# Type of support services Spoogler is looking for
+support_type = [
+    (0, "Local recommendations (i.e. for housing, health specialists, schools, etc.)"),
+    (1, "Information (i.e. on US taxes, healthcare system, building credit, visas, etc.)"),
+    (2, "Networking (based on home country, interests, current residence, etc.)"),
+    (3, "Other")]
+
+# Other types of support used by Spoogler
+support_other = [
+    (0, "Family/friends here"),
+    (1, "Country of origin or ethnicity-based groups"),
+    (2, "Faith-based groups"),
+    (3, "Other")]
+
+# Spoogler's children ages
+children_ages = [
+    (0, "No children"),
+    (1, "Expecting"),
+    (2, "Infant"),
+    (3, "Preschool, kindergarten"),
+    (4, "Elementary school"),
+    (5, "Middle school"),
+    (6, "High school"),
+    (7, "College/university"),
+    (8, "Left home")]
+
+
 class Spoogler(ndb.Model):
     full_name = ndb.StringProperty()
     spoogler_email = ndb.StringProperty()
     spoogler_fb_email = ndb.StringProperty()
     googler_ldap = ndb.StringProperty()
     spoogler_country = ndb.StringProperty()
-    work_status = ndb.StringProperty()
+    work_status = ndb.IntegerProperty(repeated=None)  # See work_status list
     engl_proficiency = ndb.StringProperty()
     spoogler_lang2 = ndb.StringProperty()
     native_lang = ndb.StringProperty()
